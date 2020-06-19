3f716980339dcf8409c42af9ef82626c7c55ded0
Dieter Halosta <dieter.halosta@yahoo.com>
2020-06-19 18:55:35 +0300

    Added a domain class and yaml config file

---
+++ src/main/java/org/fasttrackit/sportsapp/domain/Event.java
@@ -0,0 +1,101 @@
+package org.fasttrackit.sportsapp.domain;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+@Entity
+public class Event {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotNull
+    private String name;
+    @NotNull
+    private String description;
+    private int participants;
+    @NotNull
+    private LocalDate date;
+    @NotNull
+    private String location;
+    private String imageUrl;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @NotNull
+    public String getName() {
+        return name;
+    }
+
+    public void setName(@NotNull String name) {
+        this.name = name;
+    }
+
+    @NotNull
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(@NotNull String description) {
+        this.description = description;
+    }
+
+    public int getParticipants() {
+        return participants;
+    }
+
+    public void setParticipants(int participants) {
+        this.participants = participants;
+    }
+
+    @NotNull
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(@NotNull LocalDate date) {
+        this.date = date;
+    }
+
+    @NotNull
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(@NotNull String location) {
+        this.location = location;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    @Override
+    public String toString() {
+        return "Event{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", participants=" + participants +
+                ", date=" + date +
+                ", location='" + location + '\'' +
+                ", imageUrl='" + imageUrl + '\'' +
+                '}';
+    }
+}
