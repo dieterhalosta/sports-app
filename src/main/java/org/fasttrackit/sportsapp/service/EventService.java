3f4948523f2fbcc19a877e8c9015fb854854dea2
Dieter <dieter.halosta@yahoo.com>
2020-06-28 14:45:45 +0300

    Added Logger in EventService

--- src/main/java/org/fasttrackit/sportsapp/service/EventService.java
+++ src/main/java/org/fasttrackit/sportsapp/service/EventService.java
@@ -3,11 +3,15 @@ package org.fasttrackit.sportsapp.service;
 import org.fasttrackit.sportsapp.domain.Event;
 import org.fasttrackit.sportsapp.persistance.EventRepository;
 import org.fasttrackit.sportsapp.transfer.SaveEventRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EventService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(EventService.class);
 
     private final EventRepository eventRepository;
 
@@ -17,7 +21,9 @@ public class EventService {
     }
 
     public Event creatProduct(SaveEventRequest request){
-        System.out.println("Creating Product: " + request);
+//        System.out.println("Creating Product: " + request);
+
+        LOGGER.info("Creating product {}", request);
 
         Event event = new Event();
         event.setName(request.getName());
