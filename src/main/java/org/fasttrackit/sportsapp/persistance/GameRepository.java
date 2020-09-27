b775d12af8d7d3caa813de99257b9cce4d402a17
Dieter <dieter.halosta@yahoo.com>
2020-09-27 13:33:09 +0300

    Working on getting user events

--- src/main/java/org/fasttrackit/sportsapp/persistance/GameRepository.java
+++ src/main/java/org/fasttrackit/sportsapp/persistance/GameRepository.java
@@ -14,4 +14,6 @@ public interface GameRepository extends JpaRepository<Game, Long> {
     @Query("SELECT event FROM Event event WHERE (:partialName IS null OR event.name LIKE %:partialName%) AND (:dateQuery IS null or event.date = :dateQuery) AND (:partialLocation IS null or event.location LIKE %:partialLocation%)")
     Page<Event> findByOptionalCriteria(String partialName, LocalDate dateQuery, String partialLocation, Pageable pageable);
 
+    @Query("SELECT user.firstName, user.lastName, event.description, event.date, event.imageUrl, event.location, event.name, event.participants FROM User user INNER JOIN Game game ON  =user.id INNER JOIN Event event ON game_user.game_id=event.id WHERE (:activeUserId IS null or user.id LIKE %:activeUserId%)")
+    Page<Event> getUserEvents (long activeUserId, Pageable pageable);
 }
